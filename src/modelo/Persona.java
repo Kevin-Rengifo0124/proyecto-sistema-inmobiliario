62c4134fe65f600a66fd1c65ac6591065ab571e7
Kevin-Rengifo0124 <krengifo0124@gmail.com>
2025-05-08 19:30:04 -0500

    Se creo la clase persona

---
+++ src/modelo/Persona.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+public class Persona extends Usuario {
+
+    protected String nombreCompleto;
+    protected String cedula;
+    protected String telefono;
+    protected String direccion;
+
+    public Persona(String nombreCompleto, String cedula, String telefono, String direccion, String email, String clave, String rol) {
+        super(email, clave, rol);
+        this.nombreCompleto = nombreCompleto;
+        this.cedula = cedula;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+}
